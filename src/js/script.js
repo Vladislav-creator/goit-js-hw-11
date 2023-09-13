7dbeb1344a75b9f8dbd5aba80c06f4bf68eda472
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-09-13 22:21:48 +0300

    333

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,10 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './fetchImages';
-
+import {renderGallery} from './renderGallery'
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
-
+export {gallery}
 let query = '';
 let page = 1;
 let simpleLightBox;
@@ -13,52 +13,13 @@ const perPage = 40;
 
 searchForm.addEventListener('submit', onSearchForm);
 
-function renderGallery(images) {
-  // Перевірка чи існує галерея перед вставкою даних
-  if (!gallery) {
-    return;
-  }
 
-  const markup = images
-    .map(image => {
-      const {
-        id,
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = image;
-      return `
-        <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}">
-            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
-            </div>
-          </div>
-        </a>
-      `;
-    })
-    .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
+  
 
   // Цей код дозволяє автоматично прокручувати сторінку на висоту 2 карток галереї, коли вона завантажується
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
+  
 
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
 
 function onSearchForm(e) {
   e.preventDefault();
